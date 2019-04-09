351c7b431779fccb0c797f910e0d437139d4f98f
Telolahy <stephano.telolahy@gmail.com>
2019-04-09 19:08:02 +0200

    Initial Commit

---
+++ AristotePuzzle/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AristotePuzzle
+//
+//  Created by Hugues Stéphano TELOLAHY on 4/9/19.
+//  Copyright © 2019 Hugues Stéphano TELOLAHY. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
