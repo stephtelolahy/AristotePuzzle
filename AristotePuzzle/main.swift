5800833c148cbe8d45a9a5de98527569fcc350f9
Telolahy <stephano.telolahy@gmail.com>
2019-04-30 11:51:36 +0200

    add time elapsed

--- AristotePuzzle/main.swift
+++ AristotePuzzle/main.swift
@@ -8,5 +8,10 @@
 
 import Foundation
 
+let startTime = CFAbsoluteTimeGetCurrent()
+
 //MagicSquare().run(with: [1, 2, 3, 4, 5, 6, 7, 8, 9])
 MagicHexagon().run(with: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+
+let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+print("Time elapsed: \(timeElapsed) s")
